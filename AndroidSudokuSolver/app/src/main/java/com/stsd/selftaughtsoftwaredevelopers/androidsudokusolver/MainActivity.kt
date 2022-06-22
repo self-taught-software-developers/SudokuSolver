c3714bd4e1abd04c8cd3a7f299af7b03916acf45
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2022-06-22 17:40:55 -0400

    Text changed

--- AndroidSudokuSolver/app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
+++ AndroidSudokuSolver/app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Good Day $name!")
+    Text(text = "Dolphin $name!")
 }
 
 @Preview(showBackground = true)
