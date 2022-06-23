71cde97bdda894fb86324f76a2c265bc33887ad1
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2022-06-23 18:28:16 -0400

    Added Dolphin text to Greeting

--- app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
+++ app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = " Hello $name!")
+    Text(text = " Hello Dolphin $name!")
 }
 
 @Preview(showBackground = true)
