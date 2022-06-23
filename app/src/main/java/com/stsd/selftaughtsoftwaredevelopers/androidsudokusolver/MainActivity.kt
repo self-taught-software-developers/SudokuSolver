dbd16ad4b1b23cea9fbbd6ec0b409858a8657694
C-Annunziato <68083360+C-Annunziato@users.noreply.github.com>
2022-06-23 18:44:10 -0400

    just name

--- app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
+++ app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "I need help $name!")
+    Text(text = " $name!")
 }
 
 @Preview(showBackground = true)
