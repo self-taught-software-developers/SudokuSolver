fdde272e87cbcfed0a1c362ae5db0e5531b56282
cerver1 <cerve.v55@gmail.com>
2022-06-23 19:25:19 -0400

    dependency injection implementation

--- app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
+++ app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.theme.AndroidSudokuSolverTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AndroidSudokuSolverTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +30,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
 }
 
---
+++ app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/framework/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.framework.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+
+@Module
+@InstallIn(ActivityComponent::class)
+object AppModule {
+
+
+
+}
