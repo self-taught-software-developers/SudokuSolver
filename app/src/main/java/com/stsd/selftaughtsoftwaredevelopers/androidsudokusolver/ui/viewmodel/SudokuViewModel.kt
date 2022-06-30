a7b8ab51395cc2298e75d23fb24773ca4f6f25fd
cerver1 <cerve.v55@gmail.com>
2022-06-30 16:20:04 -0400

    + mutableStateList removed and sudokuBoard converted to array instead of a list. + more buttons added for undo, erase, solve + reset currently selected sudoku tile

--- app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/ui/viewmodel/SudokuViewModel.kt
+++ app/src/main/java/com/stsd/selftaughtsoftwaredevelopers/androidsudokusolver/ui/viewmodel/SudokuViewModel.kt
@@ -1,12 +1,10 @@
 package com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.viewmodel
 
 import android.util.Log
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.framework.manager.SudokuSolverWorker
 import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.model.Tile
-import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.model.sudokuBoard
+import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.model.Tile.Companion.EMPTY_TILE
 import com.stsd.selftaughtsoftwaredevelopers.androidsudokusolver.ui.model.sudokuBoardArray
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -19,12 +17,10 @@ class SudokuViewModel @Inject constructor(
 //    private val worker: SudokuSolverWorker
 ) : ViewModel() {
 
-    val sudokuBoardState = mutableStateListOf(sudokuBoard)
-
     private val _sudokuBoardStateAlt = MutableStateFlow(sudokuBoardArray)
     val sudokuBoardStateAlt : StateFlow<Array<Array<Tile>>> = _sudokuBoardStateAlt.asStateFlow()
 
-    private val _selectedPosition = MutableStateFlow(Pair(-1, -1))
+    private val _selectedPosition = MutableStateFlow(Pair(0, 0))
     val selectedPosition : StateFlow<Pair<Int, Int>> = _selectedPosition.asStateFlow()
 
     fun updateSelectedPosition(
@@ -50,21 +46,41 @@ class SudokuViewModel @Inject constructor(
             val copy = board.copyOf()
 
             copy[position.first][position.second].text = newValue
-            return@update copy
 
+            return@update copy
         }
 
     }
 
     fun unDoRecentChange() = viewModelScope.launch {
 
+        _sudokuBoardStateAlt.update { board ->
+
+            val position = selectedPosition.value
+            val copy = board.copyOf()
+
+            copy[position.first][position.second].text = EMPTY_TILE
+
+            return@update copy
+        }
+
     }
 
-    fun clearBoard() = viewModelScope.launch {
+    fun clearBoard()  {
 
+        _sudokuBoardStateAlt.update { board ->
+            val copy = board.copyOf()
+
+            copy.forEach { row ->
+                row.map { it.text = EMPTY_TILE }
+            }
+
+            return@update copy
+        }
     }
 
     fun solveBoard() = viewModelScope.launch {
+        Log.d(TAG, "solveBoard")
 
 
     }
